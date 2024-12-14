40d372ba7aef62930e24782870bdcc5347d82ba0
narim <ehdrkd648@naver.com>
2024-12-14 22:33:08 +0900

    오타 수정

--- backend/src/main/java/org/example/reservationproject/Config/ReactRouterController.java
+++ backend/src/main/java/org/example/reservationproject/Config/ReactRouterController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ReactRoutingController {
+public class ReactRouterController {
     @GetMapping(value = {"/", "/listforelicemanager", "/listforelicemanager/calender"})
     public String forward() {
         return "forward:/index.html";
