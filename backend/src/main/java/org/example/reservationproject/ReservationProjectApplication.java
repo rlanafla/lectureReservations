e06283f6b65d122acd0695784b8381ad093259bf
narim <ehdrkd648@naver.com>
2024-12-14 19:45:13 +0900

    오타 수정

--- backend/src/main/java/org/example/reservationproject/ReservationProjectApplication.java
+++ backend/src/main/java/org/example/reservationproject/ReservationProjectApplication.java
@@ -17,7 +17,7 @@ public class ReservationProjectApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://54.180.163.230:8080/);
+                registry.addMapping("/**").allowedOrigins("http://54.180.163.230:8080/");
             }
         };
     }
